e83308a7e15ec8ca4d7f86598f414bd4ea7cc8d1
Yusuf <1058297@hr.nl>
2023-04-06 12:46:47 +0200

    Admin can now add films

--- Program.cs
+++ Program.cs
@@ -5,12 +5,7 @@ class Program
     public static void Main()
     {
         CreateMenus newStart = new CreateMenus();
-<<<<<<< HEAD
-        newStart.AdminMenu();
+        newStart.AdminRemoveFilm();
         //newStart.Begin();
-=======
-        //newStart.AdminMenu();
-        newStart.Begin();
->>>>>>> f0fe1041198a583060592812d5cbe55c5a21f18d
     }
 }
