f54094d8f1837478b02425d68154989df1b4250e
Nathaniël <1056796@hr.nl>
2023-05-11 16:50:11 +0200

    change film description

--- Program.cs
+++ Program.cs
@@ -5,12 +5,9 @@ class Program
     public static void Main()
     {
         CreateMenus newStart = new CreateMenus();
-        //newStart.AdminMenu();
-<<<<<<< HEAD
-        newStart.Begin();
+        AdminMenus admin = new AdminMenus();
+        admin.AdminMenu();
+        //newStart.Begin();
         //newStart.FilmSeats();
-=======
-        newStart.FilmSeats();
->>>>>>> 462b19defcda57667559413083b518947f8a9db1
     }
 }
