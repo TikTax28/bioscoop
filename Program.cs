3732065fb808ee515ae9c8eccda871d47717c8ce
TikTax28 <tommy.manh.hung@gmail.com>
2023-05-23 01:05:34 +0200

    change film info functie gemaakt per film

--- Program.cs
+++ Program.cs
@@ -7,9 +7,9 @@ class Program
         AdminMenus admin = new AdminMenus();
         CreateMenus newStart = new CreateMenus();
         FilmMenus filmmenu = new FilmMenus();
-        //admin.AdminMenu();
+        admin.AdminMenu();
         //newStart.Begin();
         //filmmenu.FilmMenu();
-        filmmenu.FilmSeats("test1", "12-12-2003", "12:00");
+        //filmmenu.FilmSeats("test1", "12-12-2003", "12:00");
     }
 }
