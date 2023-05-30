5196e47cda9cf70eec19131e8ea8cbfda5e296e5
TikTax28 <tommy.manh.hung@gmail.com>
2023-05-30 15:11:57 +0200

    unit test tommy

--- BioscoopTest/LogicTest.cs
+++ BioscoopTest/LogicTest.cs
@@ -28,4 +28,54 @@ public class LogicTest
         Assert.Equal(newFilm.filmRoom, addedFilm.filmRoom);
         Assert.Equal(newFilm.Active, addedFilm.Active);
     }
+
+    [Fact]
+    public void TestLogin()
+    {
+        // Arrange
+        var accountsLogic = new AccountsLogic();
+        string email = "test@example.com";
+        string password = "password123";
+        accountsLogic.AddAccount(email, password, "test", false);
+
+        // Act
+        var actualAccount = accountsLogic.CheckLogin("test@example.com", "password123");
+
+        // Assert
+        Assert.Equal(email, actualAccount.EmailAddress);
+        Assert.Equal(password, actualAccount.Password);
+    }
+
+    [Fact]
+    public void TestLoginNull()
+    {
+        // Arrange
+        var accountsLogic = new AccountsLogic();
+
+        // Act
+        var actualAccount = accountsLogic.CheckLogin("test@example.com", "wrongpassword");
+
+        // Assert
+        Assert.Null(actualAccount);
+
+    }
+
+    [Fact]
+    public void TestChangeFilm()
+    {
+        var filmsLogic = new FilmsLogic();
+        string filmname = "shrek";
+        string filmdescription = "shrek the movie";
+        string filmdate = "14-06-2023";
+        string filmtime = "15:00";
+        string filmroom = "1";
+        filmsLogic.AddFilm(filmname, filmdescription, filmdate, filmtime, filmroom);
+        var addedFilm = filmsLogic.GetByDateAndTime(filmdate,filmtime);
+        Assert.Equal(filmname, addedFilm.filmName);
+
+        addedFilm.filmName = "top gun";
+        filmsLogic.UpdateFilm(addedFilm);
+        var changedFilm = filmsLogic.GetByName("top gun");
+        Assert.Equal(changedFilm.filmName, "top gun");
+    }
 }
